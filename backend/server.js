4d72fb770a255d36ef05974521ca77199ef96e72
Akriti Singh <akriti@example.com>
2026-06-24 16:07:28 +0530

    Fixed server setup

--- backend/server.js
+++ backend/server.js
@@ -5,5 +5,5 @@ const port=process.env.PORT || 3000;
 
 
  server.listen(port,()=>{
-    console.log(`server is running on the port $(port)`)
+    console.log(`server is running on the port ${port}`);
  });
